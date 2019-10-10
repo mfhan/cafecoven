f4bb5a8dcc156117411f86360e3c775f48bdcf64
mfhan <mf212mf@gmail.com>
2019-10-09 23:32:33 -0400

    fix css

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -16,29 +16,29 @@ const Intro=()=>{
       <div className = "intro">
         <h2><Icon className = 'coffee' icon={coffeeIcon} />  WHY WorkHaven?  <Icon icon={coffeeIcon} /></h2>
           <h3>IS THIS YOU?</h3>
-            <div className= "row">
-            <div className= "column">You do your best work in public places: cafeterias, independent coffee shops, hotel lobbies. But sometimes working alone gets tiresome</div>
-            <div className= "column">You are a freelancer and would like to connect with other women in your industry -- all the while getting some work done.</div>
-            <div className= "column">You LIKE the idea of writing in a coffee shop -- but you need an accountability partner who will keep you productive</div>
+            <div className= "row" id = "you">
+            <div className= "column">You do <strong>your best work</strong> in public places: cafeterias, independent coffee shops, hotel lobbies. But sometimes working alone gets tiresome</div>
+            <div className= "column">You are a <strong>freelancer</strong> and would like to <strong>connect</strong> with other women in your industry -- all the while getting some work done</div>
+            <div className= "column">You <strong>LIKE</strong> the idea of writing in a coffee shop -- but you need an <strong>accountability partner</strong> who will keep you productive</div>
             </div>
         <h3>HOW IT WORKS: SAFE, EASY, ACCURATE</h3>
-          <div className= "row">
-          <div className= "column"><Icon className ='how' icon={clipboardPencil} />SIGN UP, FILL THE FORM & INDICATE YOUR AVAILABLE TIMES </div>
-          <div className= "column"><Icon className ='how' icon={mapMarkedAlt} /> GET A MAP OF POTENTIAL MATCHES</div>
-          <div className= "column"><Icon className ='how' icon={okSign} />GET IN TOUCH WITH YOUR MATCH AND GET WORKING!</div>
+          <div className= "row" id="desc">
+          <div className= "column"><Icon  icon={clipboardPencil} />SIGN UP, FILL THE FORM & INDICATE YOUR AVAILABLE DAYS </div>
+          <div className= "column"><Icon  icon={mapMarkedAlt} /> GET A MAP OF POTENTIAL MATCHES</div>
+          <div className= "column"><Icon icon={okSign} />GET IN TOUCH WITH YOUR MATCH AND GET WORKING!</div>
           </div>
 
         <h3>TESTIMONIALS</h3>
-          <div className= "row">
+          <div className= "row"  id="praise">
           <div className= "column">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -- Julie, Minneapolis </div>
           <div className= "column">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea. -- Anne, New York</div>
           <div className= "column">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua -- Beth, Fargo, ND</div>
           </div>
         <h3>Press Coverage</h3>
-          <div className= "clips">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <div className= "row" id ="clips">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. --Elle </p>
+          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.  --Vanity Fair </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. --New York Magazine </p>
           </div>
       </div>
   );
